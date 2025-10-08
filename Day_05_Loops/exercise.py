056298e8284d8e2dbdf637f230469495df92fb70
kstejas2001 <kstejas2001@gmail.com>
2025-10-08 21:14:19 +0530

    Added Day 05 Python Loops exercise

---
+++ Day_05_Loops/exercise.py
@@ -0,0 +1,22 @@
+# Exercise 1: Print numbers 1 to 10 using a while loop.
+
+
+# Exercise 2: Print squares of numbers from 1 to 5 using a for loop.
+
+
+# Exercise 3: Write a loop that prints only odd numbers from 1 to 10.
+
+
+# Exercise 4: Use a for loop with break — stop the loop when number == 7.
+
+
+# Exercise 5: Use a for loop with continue — skip number 5 but print others (1–7).
+
+
+# Exercise 6: Nested loops pattern
+# Print:
+# *
+# **
+# ***
+# ****
+# *****
