562be87328d9aa6c1af05c7b17784d6fbea9be67
kstejas2001 <kstejas2001@gmail.com>
2025-11-03 21:46:30 +0530

    feat: Update the Day 7 list exercises

--- Day_07_Lists_Tuples/List/Exercises.py
+++ Day_07_Lists_Tuples/List/Exercises.py
@@ -1,15 +1,48 @@
 # Tiny examples
-fruits = ["apple", "banana", "mango"]
+
+# Create list elements
+fruits = ["apple", "banana", "cherry", "mango", "orange", "kiwi"]
+
+# Print the number of items in the list
+print(len(fruits)) # 6
+
+# Print the type of list
+print(type(fruits)) # <class 'list'>
+
+# Access Items
 print(fruits[0]) # apple
+print(fruits[-1]) # kiwi
+print(fruits[0:3]) # ["apple", "banana", "cherry"]
+print(fruits[:4]) # ["apple", "banana", "cherry", "mango"]
+print(fruits[2:]) # ["cherry", "mango", "orange", "kiwi"]
+print(fruits[-4:-1]) # ["cherry", "mango", "orange"]
 
-fruits.append("orange")
-print(fruits)
-fruits.insert(1, "kiwi")
-print(f"Inserting new item at the 2nd position: {fruits}")
+# Check if Item Exists
+if "apple" in fruits:
+    print("Yes, 'apple' is exist in the fruits list")
 
-print(f"List after slice: {fruits[1:4]}")
+# Modification of items
+# Change the value of a specific item
+my_list = [1, 2, 0, 4, 5]
+print(f"Before changing: {my_list}")
+my_list[2] = 3
+print(f"After change: {my_list}")
 
-print("mango" in fruits)
+# Insert items
+my_list.insert(0, 0)
+print(f"Insert new item: {my_list}")
+
+# Append() Items
+my_list.append(6)
+print(f"After append: {my_list}")
+
+# Remove List
+# Remove Specified Item
+my_list.remove(0)
+print(f"Removing the first item {my_list}")
+
+
+
 
 # Implement the following:
 
