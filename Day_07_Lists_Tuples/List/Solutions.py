4389b4897bfc60dde1847721fca5cbf2ccf6acba
kstejas2001 <kstejas2001@gmail.com>
2025-11-06 20:43:28 +0530

    feat: Added solutions for Day 7 Python list

--- Day_07_Lists_Tuples/List/Solutions.py
+++ Day_07_Lists_Tuples/List/Solutions.py
@@ -1,18 +1,18 @@
 # Solution 1: Create the initial list 'nums' containing numbers 5, 2, 9, 1, 5.
 nums = [5, 2, 9, 1, 5]
-print(f"The list are: {nums}")
+print(f"The list are: {nums}") # [5, 2, 9, 1, 5]
 
-# Solution 2: Print its length of the list
-print(f"The length of list is: {len(nums)}")
+# Solution 2: Print its length.
+print(f"The length of list is: {len(nums)}") # 5
 
 # Solution 3: Add the number '7' to the end.
 nums.append(7)
-print(f"After adding the new element(7) at the end: {nums}")
+print(f"After adding the new element(7) at the end: {nums}") # [5, 2, 9, 1, 5, 7]
 
 # Solution 4: Remove the first occurrence '5' from the list.
-nums.remove(5) # or pop(0)
-print(f"After remove the first '5' item: {nums}")
+nums.remove(5)
+print(f"After remove the first '5' item: {nums}") # [2, 9, 1, 5, 7]
 
 # Solution 5: Sort the list in ascending order and print the result.
 nums.sort()
-print(f"Sorted list: {nums}")
+print(f"Sorted list: {nums}") # [1, 2, 5, 7, 9]
