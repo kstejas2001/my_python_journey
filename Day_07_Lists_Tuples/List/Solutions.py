95dcb9352bfb9a0295b0b42af7d79ccce7e98282
kstejas2001 <kstejas2001@gmail.com>
2025-11-08 14:47:34 +0530

    feat: Added solutions for Day 7 Python list

--- Day_07_Lists_Tuples/List/Solutions.py
+++ Day_07_Lists_Tuples/List/Solutions.py
@@ -20,16 +20,35 @@ print(f"Sorted list: {nums}") # [1, 2, 5, 7, 9]
 
 
 # Exercise 2 (Nested Lists)
-# Solution 1:
+# 1: Create the nested list.
+
 matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
-print(f"The Nested list are: {matrix}")
+print(f"The Nested list are: {matrix}") # [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
 
-# Solution 2:
-print(f"row 2 lists elements are: {matrix[1]}")
-print(f"column 3 lists elements are: [{matrix[0][2]}], [{matrix[2][2]}]")
+# 2: Access and print the element at row 2, column 3.
 
-# Solution 3:
+# Access the element at row 2 (index 1), column 3 (index 2).
+element = matrix[1][2]
+print(f"The element at row 2, column 3 is: {element}") # expected output: 6
+
+# 3: Use nested loops to print every element on a new line.
+# Nested 'for' loops can be used to iterate through each sublist(row) and then each item within that sublist (column).
+
+# Iterate though each row in the matrix.
 for row in matrix:
+    # Iterate through each element in the current row.
     for element in row:
-        print(element, end=" ")
-    print()
+        # Format the output string.
+        print(f"Element: {element}")
+
+
+# Exercise 3 — List Comprehension
+# 1. Create the list from 1 to 100 using list comprehension.
+squares = [x**2 for x in range(1, 11)]
+
+# 2. Print the list.
+print(squares)
+
+# 3. For even numbers
+even_squares = [x for x in squares if x % 2 == 0]
+print(even_squares)
